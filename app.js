d3a666396682fca550c87a46942d67f764c1cb30
Daniel Lu <ludaniel20110@outlook.com>
2022-08-13 04:40:35 +0000

    make faster

--- app.js
+++ app.js
@@ -56,3 +56,11 @@ const snow = function() {
         }
     }());
 }
+
+function refreshTime() {
+    const timeDisplay = document.getElementById("time");
+    const dateString = new Date().toLocaleString();
+    const formattedString = dateString.replace(", ", " - ");
+    timeDisplay.textContent = formattedString;
+}
+setInterval(refreshTime, 1000);
--- time.js
+++
@@ -1,7 +0,0 @@
-function refreshTime() {
-    const timeDisplay = document.getElementById("time");
-    const dateString = new Date().toLocaleString();
-    const formattedString = dateString.replace(", ", " - ");
-    timeDisplay.textContent = formattedString;
-}
-setInterval(refreshTime, 1000);
